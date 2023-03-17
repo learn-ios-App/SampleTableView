ddd3960567123f4c41e74d9cd9e3b9a732f816c2
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-03-18 01:22:58 +0900

    Initial Commit

---
+++ SampleTableView-Part2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleTableView-Part2
+//
+//  Created by 渡邊魁優 on 2023/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
